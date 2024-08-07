f301811a9321c17c791df6e6d6c9586437298180
Thiago Cardoso <cafeprathi@gmail.com>
2024-08-06 23:21:52 -0300

    cars

--- cars/models.py
+++ cars/models.py
@@ -5,6 +5,7 @@ class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
     brand = models.CharField(max_length=200)
-    factory_year = models.IntegerField(black=True, null=True)
+    factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
+
