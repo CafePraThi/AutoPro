89d4a258b18808beba5a63587773ce746487d821
Thiago Cardoso <cafeprathi@gmail.com>
2024-08-08 23:39:31 -0300

    car midia

--- cars/admin.py
+++ cars/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car, Brands
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
     search_fields = ('model', 'brand')
 
+class BrandsAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
 admin.site.register(Car, CarAdmin)
+admin.site.register(Brands, BrandsAdmin)
