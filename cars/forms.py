7b560dbee1eeabd2351e3a0b6ef76dfeeada4d5d
Thiago Cardoso <cafeprathi@gmail.com>
2024-08-11 22:33:30 -0300

    validation

--- cars/forms.py
+++ cars/forms.py
@@ -1,24 +1,28 @@
 from django import forms
-from cars.models import Brands, Car
+from cars.models import Car
+    
+class CarForms(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = '__all__'
 
-class CarForms(forms.Form):
-    model = forms.CharField(max_length=200)
-    brand = forms.ModelChoiceField(Brands.objects.all())
-    factory_year = forms.IntegerField()
-    model_year = forms.IntegerField()
-    plate = forms.CharField(max_length=10)
-    value = forms.FloatField()
-    photo = forms.ImageField()
-
-    def save(self):
-        new_car = Car(
-            model = self.cleaned_data['model'],
-            brand = self.cleaned_data['brand'],
-            factory_year = self.cleaned_data['factory_year'],
-            model_year = self.cleaned_data['model_year'],
-            plate = self.cleaned_data['plate'],
-            value = self.cleaned_data['value'],
-            photo = self.cleaned_data['photo']
-        )
-        new_car.save()
-        return new_car
+        def clean_value(self):
+            value = self.cleaned_data.get('value')
+            if value < 15000:
+                self.add_error('value', f'The value of the car must be greater than {value}')
+            return value
+        
+        def clean_factory_year(self):
+            factory_year = self.cleaned_data.get('factory_year')
+            if len(str(factory_year)) != 4:
+                self.add_error('factory_year', 'The factory year must have 4 digits')
+            if factory_year < 1900:
+                self.add_error('factory_year', f'The factory year must be greater than {factory_year}')
+            return factory_year
+        
+        def clean_model_year(self):
+            model_year = self.cleaned_data.get('model_year')
+            if len(str(model_year)) != 4:
+                self.add_error('model_year', 'The model year must have 4 digits')
+            return model_year
+        
