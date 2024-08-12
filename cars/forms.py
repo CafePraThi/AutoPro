3cf82ccaddc414a0adcdd4c645a95dddea9d8b9f
Thiago Cardoso <cafeprathi@gmail.com>
2024-08-11 21:04:44 -0300

    formsCarnew

--- cars/forms.py
+++ cars/forms.py
@@ -4,7 +4,7 @@ from cars.models import Brands, Car
 class CarForms(forms.Form):
     model = forms.CharField(max_length=200)
     brand = forms.ModelChoiceField(Brands.objects.all())
-    factor_year = forms.IntegerField()
+    factory_year = forms.IntegerField()
     model_year = forms.IntegerField()
     plate = forms.CharField(max_length=10)
     value = forms.FloatField()
@@ -14,7 +14,7 @@ class CarForms(forms.Form):
         new_car = Car(
             model = self.cleaned_data['model'],
             brand = self.cleaned_data['brand'],
-            factor_year = self.cleaned_data['factor_year'],
+            factory_year = self.cleaned_data['factory_year'],
             model_year = self.cleaned_data['model_year'],
             plate = self.cleaned_data['plate'],
             value = self.cleaned_data['value'],
