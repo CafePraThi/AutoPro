6381674ff9015223e540b6f59c2d51ac5549b7be
Thiago Cardoso <cafeprathi@gmail.com>
2024-08-11 23:26:05 -0300

    register

--- core/urls.py
+++ core/urls.py
@@ -24,4 +24,5 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cars.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
