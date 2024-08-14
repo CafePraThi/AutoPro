fc4ee4836ef13dedc8c6e0ce60ac3b47a7d7752a
Thiago Cardoso <cafeprathi@gmail.com>
2024-08-13 23:38:16 -0300

    listview

--- cars/urls.py
+++ cars/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from cars.views import CarView
-from cars import views
+from cars.views import  CarsList, NewCarCreateView
 
 app_name = 'cars'
 
 urlpatterns = [
-    path('cars/', CarView.as_view(), name='cars_view'),
-    path('cars/add/', views.add_car_view, name='add_car_view'),
+    path('cars/', CarsList.as_view(), name='cars_view'),
+    path('cars/add/', NewCarCreateView.as_view(), name='add_car_view'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -3,27 +3,24 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarForms
 from django.views import View
+from django.views.generic import ListView, CreateView
 
 
-class CarView(View):
-    def get(self, request):
-        cars = Car.objects.all()
-        search = request.GET.get('search', None)
+class CarsList(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
 
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
         if search:
-            cars = Car.objects.filter(model_icontains=search).order_by('-id')
-            
-        return render(request, 'cars.html', {'cars': cars})
+            cars = cars.filter(model__icontains=search)
+        return cars
+    
 
-
-def add_car_view(request):
-    if request.method == 'POST':
-        new_car_form = CarForms(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars:cars_view')
-
-    else:
-        new_car_form = CarForms()
-
-    return render(request, 'add_car.html', {'new_car_form': new_car_form})
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarForms
+    template_name = 'add_car.html'
+    success_url = '/cars/'
